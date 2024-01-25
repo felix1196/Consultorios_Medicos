1fd88d293b4b22d7655f6ab9f4e18c8f913c1bda
Felix <felixjr11@hotmail.com>
2024-01-24 23:49:59 -0600

    Add script to registered empleados.

--- DBScripts/CM_RegistrarEmpleado.sql
+++ DBScripts/CM_RegistrarEmpleado.sql
@@ -8,13 +8,49 @@ CREATE PROCEDURE [dbo].[CM_RegistrarEmpleado]
 @correo					VARCHAR(255),
 @fechaNacimiento		DATETIME,
 @usuario				VARCHAR(10),
-@contraseña				VARCHAR(10)
-
-
+@contraseña				VARCHAR(10),
+@tipoUsuario			VARCHAR(255),
+@Mensaje				VARCHAR(MAX) OUTPUT
 AS
 BEGIN
-	SELECT
-		*
-	FROM
-		Empleados WITH(NOLOCK)
+	
+	DECLARE @idTipoUsuario INT
+
+	IF EXISTS(SELECT TOP 1 1 FROM Empleados WITH(NOLOCK) WHERE Usuario = @usuario AND Contraseña = @contraseña)
+	BEGIN
+		SET @Mensaje = 'Error, no se puede ingresar el siguiente empleado el usuario y/o contraseña ya existen.'
+	END
+	ELSE
+	BEGIN
+		SELECT
+			@idTipoUsuario = ID
+		FROM
+			Empleados_Tipo WITH(NOLOCK)
+		WHERE
+			Descripcion = @tipoUsuario
+
+		INSERT INTO Empleados
+		(
+			Nombre,
+			Telefono,
+			Correo,
+			FechaNacimiento,
+			Usuario,
+			Contraseña,
+			IdTipoUsuario,
+			FechaRegistro
+			
+		)
+		VALUES
+		(
+			@nombre,
+			@telefono,
+			@correo,
+			@fechaNacimiento,
+			@usuario,
+			@contraseña,
+			@idTipoUsuario,
+			GETDATE()
+		)
+	END
 END
